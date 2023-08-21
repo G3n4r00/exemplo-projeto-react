3624c27d4557700f8f787598889eb28c4b79c2c3
G3n4r00 <rm551986@fiap.com.br>
2023-08-21 10:02:25 -0300

    primeiros passos react

---
+++ src/components/Menu.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Menu() {
+  return (
+    <ul>
+        <li>FIAP</li>
+        <li>Google</li>
+        <li>Facebook</li>
+        <li>Twitter</li>
+        <li>Amazon</li>
+    </ul>
+  )
+}
